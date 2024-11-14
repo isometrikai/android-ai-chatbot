258149660896ee5926d1cf6c690ff68b6db430c4
mayur. Dhabhi <mayur.dhabhi@appscrip.co>
2024-11-14 17:03:53 +0530

    - Down below version - remove test dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,6 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation(project(":androidChatBot"))
